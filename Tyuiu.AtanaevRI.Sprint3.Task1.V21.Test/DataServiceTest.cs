fc8ab8eae76d9703133dfee6ee572a4626affb58
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-30 21:49:29 +0500

    Task1

---
+++ Tyuiu.AtanaevRI.Sprint3.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AtanaevRI.Sprint3.Task1.V21.Lib;
+namespace Tyuiu.AtanaevRI.Sprint3.Task1.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            int stopvalue = 12;
+            int startvalue = 1;
+            
+            double res = ds.GetMultiplySeries(value, startvalue ,stopvalue);
+            Assert.AreEqual(0.019,res);
+
+        }
+    }
+}
---
+++ Tyuiu.AtanaevRI.Sprint3.Task1.V21/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AtanaevRI.Sprint3.Task1.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();    
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+   
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int value = 1;
+        int startValue = 1;
+        int stopValue = 12;
+        Console.WriteLine(ds.GetMultiplySeries( value ,startValue,stopValue));
+    }
+}
