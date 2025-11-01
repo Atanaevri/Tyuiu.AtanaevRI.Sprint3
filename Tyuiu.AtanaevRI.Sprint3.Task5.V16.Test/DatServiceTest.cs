d6d76df595ac16c504f667535b41fd3ed1be3bc4
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-11-01 12:19:46 +0500

    Task5

---
+++ Tyuiu.AtanaevRI.Sprint3.Task5.V16.Test/DatServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AtanaevRI.Sprint3.Task5.V16.Lib;
+namespace Tyuiu.AtanaevRI.Sprint3.Task5.V16.Test
+{
+    [TestClass]
+    public sealed class DatServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            Assert.AreEqual(122,res);
+
+        }
+    }
+}
---
+++ Tyuiu.AtanaevRI.Sprint3.Task5.V16/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AtanaevRI.Sprint3.Task5.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 2;
+        int startValue1 = 1;
+        int stopValue1 = 3;
+        int startValue2 = 1;
+        int stopValue2 = 10;
+        Console.WriteLine(ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+    }
+}
