10a1ad709fb5596c77d975c9ec5fddfda08ba215
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-11-01 11:46:10 +0500

    Task4

---
+++ Tyuiu.AtanaevRI.Sprint3.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AtanaevRI.Sprint3.Task4.V11.Lib;
+namespace Tyuiu.AtanaevRI.Sprint3.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            Assert.AreEqual(0.104,res);
+        }
+    }
+}
---
+++ Tyuiu.AtanaevRI.Sprint3.Task4.V11/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AtanaevRI.Sprint3.Task4.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine(ds.Calculate( startValue, stopValue));
+    }
+}
